59e653f4e07d3a7aaeb324888fd74d32415636bd
Rebecca <rebecca@heyman.com>
2022-02-01 13:33:48 -0600

    add addTodo and change state based on new item added to array

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react'
 import { StyleSheet, Text, View, FlatList } from 'react-native';
 import Header from './components/header';
 import TodoItem from './components/todoItem';
+import AddTodo from './components/addTodo';
 
 export default function App() {
   const [todos, setTodos] = useState([
@@ -16,11 +17,20 @@ export default function App() {
     })
   }
 
+  const submitHandler = (text) => {
+    setTodos((prevTodos)=> {
+      return [
+        {text: text, key: (prevTodos.length + 1).toString()},
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* todo form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
